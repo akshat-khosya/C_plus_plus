bbcde9d342ab997d5d706e914f206253e56c2216
akshat-khosya <20106@iiitu.ac.in>
2021-11-15 11:35:54 +0530

    cheching everything uptodate

---
+++ Binary Tree/binaryTreeInputOutput.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+template <typename T>
+class BinaryTreeNode {
+    T data;
+    BinaryTreeNode* leftNode;
+    BinaryTreeNode* rightNode;
+    BinaryTreeNode(T data){
+        this->data=data;
+        leftNode=NULL;
+        rightNode=NULL;
+    }
+};
+int main()
+{
+return 0;
+}
